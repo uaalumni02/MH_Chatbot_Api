1715ca225983af404df7a7ccd8d6796d47a59af5
uaalumni02 <dbell1911@gmail.com>
2025-03-06 18:18:22 -0600

    create chat model

---
+++ src/models/chat.js
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+const { Schema } = mongoose;
+
+const chatInformationSchema = Schema({
+  userName: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+  },
+  prompt: {
+    type: Schema.Types.Mixed,
+    required: [true, "prompt is required"],
+  },
+
+  __v: {
+    type: Number,
+    select: false,
+  },
+});
+
+export default mongoose.model("Chat", chatInformationSchema);
