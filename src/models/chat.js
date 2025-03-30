ea27e79d814fb250a4603a5b144d18aaaefe4bee
uaalumni02 <dbell1911@gmail.com>
2025-03-30 17:03:10 -0400

    add timestamp

--- src/models/chat.js
+++ src/models/chat.js
@@ -1,27 +1,31 @@
 import mongoose from "mongoose";
 const { Schema } = mongoose;
 
-const chatInformationSchema = Schema({
-  userName: {
-    type: Schema.Types.ObjectId,
-    ref: "User",
+const chatInformationSchema = Schema(
+  {
+    userName: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+    prompt: {
+      type: Schema.Types.Mixed,
+      required: [true, "prompt is required"],
+    },
+    response: {
+      type: String,
+    },
+    mood: {
+      type: Schema.Types.Mixed, // Ensure it can store objects
+      required: false,
+    },
+    __v: {
+      type: Number,
+      select: false,
+    },
   },
-  prompt: {
-    type: Schema.Types.Mixed,
-    required: [true, "prompt is required"],
-  },
-  response: {
-    type: String,
-  },
-  mood: {
-    type: Schema.Types.Mixed, // Ensure it can store objects
-    required: false,
-  },
-
-  __v: {
-    type: Number,
-    select: false,
-  },
-});
+  {
+    timestamps: true, 
+  }
+);
 
 export default mongoose.model("Chat", chatInformationSchema);
